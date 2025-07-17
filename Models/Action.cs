e406d7c29ca955da47818dc12468f2202ce1db21
muskandhull <muskandhull@kgpian.iitkgp.ac.in>
2025-07-18 02:29:18 +0530

    Create Action.cs

---
+++ Models/Action.cs
@@ -0,0 +1,11 @@
+namespace WorkflowEngine.Models;
+using System.Collections.Generic;
+
+public class WorkflowAction
+{
+    public string Id { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public bool Enabled { get; set; } = true;
+    public List<string> FromStates { get; set; } = new();
+    public string ToState { get; set; } = default!;
+}
